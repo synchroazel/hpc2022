24898243a1a64b9f1a7d2ef910a14e77754b65ae
synchroazel <antonio.padalino.me@gmail.com>
2022-10-20 12:32:34 +0200

    updated .gitignore and removed unnecessary files

--- benchmarking/plot_data.py
+++ benchmarking/plot_data.py
@@ -1,15 +1,22 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import math
 
 df = pd.read_csv("data.tsv", delimiter="\t")
 
-plt.plot(df["n"], df["time (sec)"])
+time = df["time (sec)"].astype(float)
+
+plt.plot(df["n"], [math.log(t) for t in time])
+# plt.plot(df['size'], time)
 plt.title("Avg. trip time over buffer size")
 plt.xlabel("buffer size")
 plt.ylabel("avg. trip time (sec)")
 plt.show()
 
-plt.plot(df["n"], df["rate (MB/sec)"])
+rate = df["rate (MB/sec)"].astype(float)
+
+plt.plot(df["n"], [math.log(r) for r in rate])
+# plt.plot(df["n"], rate)
 plt.title("Avg. rate over buffer size")
 plt.ylabel("avg. rate (MB/sec)")
 plt.xlabel("buffer size")
