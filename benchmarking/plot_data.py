99dd7907f9b0d95a0cff56ffa30e7b5a366c1851
synchroazel <antonio.padalino.me@gmail.com>
2022-10-20 12:58:26 +0200

    updated plot_data.py and added first data produced by the cluster

--- benchmarking/plot_data.py
+++ benchmarking/plot_data.py
@@ -1,23 +1,41 @@
-import matplotlib.pyplot as plt
-import pandas as pd
+import argparse
 import math
 
-df = pd.read_csv("data.tsv", delimiter="\t")
+import matplotlib.pyplot as plt
+import pandas as pd
 
-time = df["time (sec)"].astype(float)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Plot rate and time data")
+    parser.add_argument("-p", "--path", type=str, default="data.tsv", help="Path of the .tsv file generated")
+    parser.add_argument("-l", "--log-scale", action="store_true", default=False, help="Display data in log scale")
 
-plt.plot(df["n"], [math.log(t) for t in time])
-# plt.plot(df['size'], time)
-plt.title("Avg. trip time over buffer size")
-plt.xlabel("buffer size")
-plt.ylabel("avg. trip time (sec)")
-plt.show()
+    args = parser.parse_args()
 
-rate = df["rate (MB/sec)"].astype(float)
+    df = pd.read_csv(args.path, delimiter="\t")
 
-plt.plot(df["n"], [math.log(r) for r in rate])
-# plt.plot(df["n"], rate)
-plt.title("Avg. rate over buffer size")
-plt.ylabel("avg. rate (MB/sec)")
-plt.xlabel("buffer size")
-plt.show()
+    fig, axs = plt.subplots(1, 2)
+    fig.set_figwidth(10)
+
+    title = "Trip time and Rate" if not args.log_scale else "Trip time and Rate (log scale)"
+    fig.suptitle(title)
+
+    time = df["time (sec)"].astype(float)
+    rate = df["rate (MB/sec)"].astype(float)
+
+    if args.log_scale == True:
+        axs[0].plot(df["n"], [math.log(t) for t in time])
+        axs[1].plot(df["n"], [math.log(r) for r in rate])
+
+    else:
+        axs[0].plot(df["n"], time)
+        axs[1].plot(df["n"], rate)
+
+    axs[0].set_title("Avg. trip time over buffer size")
+    axs[0].set_xlabel("buffer size")
+    axs[0].set_ylabel("avg. trip time (sec)")
+
+    axs[1].set_title("Avg. rate over buffer size")
+    axs[1].set_ylabel("avg. rate (MB/sec)")
+    axs[1].set_xlabel("buffer size")
+
+    plt.show()
