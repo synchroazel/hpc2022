dbcf5970ffda5bc68ea59b33f29e74ad0877b35d
Antonio Padalino <antonio.padalino.me@gmail.com>
2022-12-04 20:21:11 +0100

    updated submit.sh

--- submit.sh
+++ submit.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=2:mem=2gb
-#PBS -l walltime=0:01:00
+#PBS -l select=4:ncpus=16:mem=4gb
+#PBS -l walltime=0:60:00
 #PBS -q short_cpuQ
 
 module load mpich-3.2
 
-mpirun.actual -np 64 ./cmake-build-debug/hpc2022 -l tuning \
-                                   -i /antonio.padalino/hpc2022/data/iris_train.csv \
-                                   -I /antonio.padalino/hpc2022/data/iris_validation.csv \
-                                   -H /antonio.padalino/hpc2022/hyperparameters.json \
+export PERFORMANCE_CHECKS=TRUE
+
+mpirun.actual -np 64 ~/hpc2022/cmake-build-debug/hpc2022 -l tuning \
+                                   -i ~/hpc2022/data/iris_train.csv \
+                                   -I ~/hpc2022/data/iris_validation.csv \
+                                   -H ~/hpc2022/hyperparameters.json \
                                    -r 70 -R 30 -c 5 -t 5
