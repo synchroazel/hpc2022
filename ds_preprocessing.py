5dfcfe4f861fc67f4398b2b65cb319ff619fd22a
Antonio Padalino <antonio.padalino.me@gmail.com>
2022-12-04 20:00:45 +0100

    added python script to preprocess dataset

---
+++ ds_preprocessing.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import argparse
+import os
+
+
+def load_data(path):
+    df = pd.read_csv(path, sep=',', header=None, low_memory=False, error_bad_lines=False)
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    df_training = df.iloc[:int(len(df) * 0.7)]
+    df_validation = df.iloc[int(len(df) * 0.7):int(len(df) * 0.9)]
+    df_test = df.iloc[int(len(df) * 0.9):]
+
+    basename = os.path.basename(path).replace('.csv',"")
+    dir = os.path.dirname(path)
+
+    df_training.to_csv(f'{dir}/{basename}_training.csv', header=False, index=False)
+    df_validation.to_csv(f'{dir}/{basename}_validation.csv', header=False, index=False)
+    df_test.to_csv(f'{dir}/{basename}_test.csv', header=False, index=False)
+
+    print('[INFO] Training dataset has {} rows and {} columns'.format(df_training.shape[0], df_training.shape[1]))
+    print('[INFO] Validation dataset has {} rows and {} columns'.format(df_validation.shape[0], df_validation.shape[1]))
+    print('[INFO] Test dataset has {} rows and {} columns'.format(df_test.shape[0], df_test.shape[1]))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Get the path to the dataset to preprocess.')
+    parser.add_argument('--filename', '-f', type=str, help='Path to the dataset to preprocess.')
+
+    args = parser.parse_args()
+
+    filename = args.filename
+
+    load_data(filename)
