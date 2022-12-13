4fd644d966c4e4e3fec430498defc7ca76efac43
Antonio Padalino <89596303+synchroazel@users.noreply.github.com>
2022-12-13 16:58:35 +0100

    Small bug fix

--- submit.sh
+++ submit.sh
@@ -9,7 +9,7 @@ module load mpich-3.2
 export PERFORMANCE_CHECKS=TRUE
 
 mpirun.actual -np 64 ~/hpc2022/cmake-build-debug/hpc2022 -l tuning \
-                                   -i ~/hpc2022/data/iris_train.csv \
+                                   -i ~/hpc2022/data/iris_training.csv \
                                    -I ~/hpc2022/data/iris_validation.csv \
                                    -H ~/hpc2022/hyperparameters.json \
                                    -r 70 -R 30 -c 5 -t 5
